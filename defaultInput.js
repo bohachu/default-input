fbe5ba5440d3ba2a04eabfdf65b2daa383aeaf89
bohachu <cbh@cameo.tw>
2023-04-08 07:57:08 +0800

    Create defaultInput.js

---
+++ defaultInput.js
@@ -0,0 +1,34 @@
+// defaultInput.js
+class DefaultInput {
+  constructor() {
+    this.setDefaultInputValue = this.setDefaultInputValue.bind(this);
+  }
+
+  setDefaultInputValue(inputElement) {
+    const inputId = inputElement.id;
+
+    if (!inputElement) {
+      console.error(`Input element with id '${inputId}' not found.`);
+      return;
+    }
+
+    const defaultValue = '';
+    const savedInputValue = localStorage.getItem(inputId) || defaultValue;
+    inputElement.value = savedInputValue;
+
+    inputElement.addEventListener('change', () => {
+      localStorage.setItem(inputId, inputElement.value);
+    });
+  }
+
+  init() {
+    document.addEventListener('DOMContentLoaded', () => {
+      const inputElements = document.querySelectorAll('input');
+      inputElements.forEach(inputElement => {
+        this.setDefaultInputValue(inputElement);
+      });
+    });
+  }
+}
+
+export default DefaultInput;
