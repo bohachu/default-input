2d03ca0bda497f6c4790f225808cfbc45473c068
bohachu <cbh@cameo.tw>
2023-04-08 08:13:01 +0800

    Update defaultInput.js

--- defaultInput.js
+++ defaultInput.js
@@ -1,34 +1,37 @@
 // defaultInput.js
-class DefaultInput {
-  constructor() {
-    this.setDefaultInputValue = this.setDefaultInputValue.bind(this);
-  }
-
-  setDefaultInputValue(inputElement) {
-    const inputId = inputElement.id;
-
-    if (!inputElement) {
-      console.error(`Input element with id '${inputId}' not found.`);
-      return;
+(function() {
+  class DefaultInput {
+    constructor() {
+      this.setDefaultInputValue = this.setDefaultInputValue.bind(this);
     }
 
-    const defaultValue = '';
-    const savedInputValue = localStorage.getItem(inputId) || defaultValue;
-    inputElement.value = savedInputValue;
+    setDefaultInputValue(inputElement) {
+      const inputId = inputElement.id;
 
-    inputElement.addEventListener('change', () => {
-      localStorage.setItem(inputId, inputElement.value);
-    });
-  }
+      if (!inputElement) {
+        console.error(`Input element with id '${inputId}' not found.`);
+        return;
+      }
 
-  init() {
-    document.addEventListener('DOMContentLoaded', () => {
-      const inputElements = document.querySelectorAll('input');
-      inputElements.forEach(inputElement => {
-        this.setDefaultInputValue(inputElement);
+      const defaultValue = '';
+      const savedInputValue = localStorage.getItem(inputId) || defaultValue;
+      inputElement.value = savedInputValue;
+
+      inputElement.addEventListener('change', () => {
+        localStorage.setItem(inputId, inputElement.value);
       });
-    });
-  }
-}
+    }
 
-export default DefaultInput;
+    init() {
+      document.addEventListener('DOMContentLoaded', () => {
+        const inputElements = document.querySelectorAll('input');
+        inputElements.forEach(inputElement => {
+          this.setDefaultInputValue(inputElement);
+        });
+      });
+    }
+  }
+
+  const defaultInput = new DefaultInput();
+  defaultInput.init();
+})();
